994fb38e490fab746ab9e9253c06be7c86649ce7
Perspicacious <johnayomide920@gmail.com>
2024-09-08 05:22:16 +0100

    m

--- components/OverlayAlert.js
+++ components/OverlayAlert.js
@@ -11,8 +11,8 @@ const OverlayAlert = () => {
   return (
     <>
       {isVisible && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center !important">
-          <div className="bg-white p-4 rounded-lg relative max-w-md mx-auto md:max-w-lg lg:max-w-2xl !important">
+        <div className="fixed inset-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-index-1">
+          <div className="bg-white p-4 rounded-lg relative max-w-md mx-auto md:max-w-lg lg:max-w-2xl">
             <img
               src="/assets/close.svg"
               alt="Close"
@@ -21,12 +21,12 @@ const OverlayAlert = () => {
             />
             <img src="/logo white.png" alt="Hubpost Logo" className="w-24 h-24 mb-1" />
             <p className="text-lg font-bold md:w-full md:flex md:flex-col md:items-center">
-  Please re-upload your profile picture to complete your profile changes.
-  <br className="md:inline" />
-  This ensures your updated information is accurately reflected.
-  <br className="md:inline" />
-  <span className="text-green-600">(Note: This step is necessary even if you've uploaded a picture before)</span>
-</p>
+              Please re-upload your profile picture to complete your profile changes.
+              <br className="md:inline" />
+              This ensures your updated information is accurately reflected.
+              <br className="md:inline" />
+              <span className="text-green-600">(Note: This step is necessary even if you've uploaded a picture before)</span>
+            </p>
           </div>
         </div>
       )}
